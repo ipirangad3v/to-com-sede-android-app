a1eb1f8721f43f32cf26fadf5f80fc1349e9f114
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2023-05-05 13:18:42 -0300

    added total to cart

--- app/src/main/java/com/ipsoft/tocomsede/utils/Cart.kt
+++ app/src/main/java/com/ipsoft/tocomsede/utils/Cart.kt
@@ -1,16 +1,21 @@
 package com.ipsoft.tocomsede.utils
 
+import com.ipsoft.tocomsede.core.extensions.toCurrency
 import com.ipsoft.tocomsede.core.model.Item
 
 object Cart {
     private val items = linkedSetOf<Pair<Item, Int>>()
 
+    fun getTotal() = items.sumOf { it.first.price * it.second }.toString().toCurrency()
+
     fun addItem(item: Item, quantity: Int) {
         items.add(item to quantity)
     }
+
     fun removeItem(item: Item) {
         items.removeIf { it.first.id == item.id }
     }
+
     fun getItems() = items
     fun clearCart() {
         items.clear()
