b94ba6c9e4c4bd86af9ccb0d03df80c569f2d735
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2023-05-06 14:11:34 -0300

    added internet validation

--- app/src/main/java/com/ipsoft/tocomsede/core/extensions/Context.kt
+++ app/src/main/java/com/ipsoft/tocomsede/core/extensions/Context.kt
@@ -1,8 +1,13 @@
 package com.ipsoft.tocomsede.core.extensions
 
 import android.content.Context
+import android.net.ConnectivityManager
 import android.widget.Toast
 
 fun Context.showMsg(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
+
+val Context.connectivityManager: ConnectivityManager
+    get() =
+        this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
--- app/src/main/java/com/ipsoft/tocomsede/home/ui/HomeScreen.kt
+++ app/src/main/java/com/ipsoft/tocomsede/home/ui/HomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -18,20 +19,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.ipsoft.tocomsede.R
 import com.ipsoft.tocomsede.core.ui.theme.gray
+import com.ipsoft.tocomsede.data.network.NetworkHandler
 
 @Composable
 fun HomeScreen(
     homeViewModel: HomeViewModel = hiltViewModel(),
     navController: NavHostController
 ) {
+    val context = LocalContext.current
+
+    val hasInternet = remember {
+        mutableStateOf(NetworkHandler(context).isNetworkAvailable())
+    }
     val categoryState = homeViewModel.categories.value
 
     val visibility = remember { mutableStateOf(false) }
@@ -41,45 +50,68 @@ fun HomeScreen(
             .fillMaxSize(),
         color = gray
     ) {
-        categoryState.error?.let {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Center
-            ) {
-                Text(text = it)
-                Spacer(modifier = Modifier.padding(8.dp))
-                Button(
-                    onClick = { homeViewModel.loadItems() },
-                    modifier = Modifier.wrapContentSize()
+        if (hasInternet.value) {
+            categoryState.error?.let {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Center
                 ) {
-                    Text(text = stringResource(id = R.string.try_again))
+                    Text(text = it)
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    Button(
+                        onClick = { homeViewModel.loadItems() },
+                        modifier = Modifier.wrapContentSize()
+                    ) {
+                        Text(text = stringResource(id = R.string.try_again))
+                    }
                 }
             }
-        }
 
-        if (categoryState.isLoading) {
+            if (categoryState.isLoading) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Center
+                ) {
+                    CircularProgressIndicator(modifier = Modifier.wrapContentSize())
+                }
+            } else {
+                visibility.value = true
+                AnimatedVisibility(
+                    visible = visibility.value,
+                    enter = fadeIn() + slideInVertically(),
+                    exit = slideOutVertically() + fadeOut(),
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        LazyColumn {
+                            categoryState.item.forEach { category ->
+                                item {
+                                    HomeCategoryList(
+                                        category = category,
+                                        navController = navController
+                                    )
+                                }
+                                item { Spacer(modifier = Modifier.padding(8.dp)) }
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
             Box(
                 modifier = Modifier.fillMaxSize(),
                 contentAlignment = Center
             ) {
-                CircularProgressIndicator(modifier = Modifier.wrapContentSize())
-            }
-        } else {
-            visibility.value = true
-            AnimatedVisibility(
-                visible = visibility.value,
-                enter = fadeIn() + slideInVertically(),
-                exit = slideOutVertically() + fadeOut(),
-                modifier = Modifier.fillMaxSize()
-            ) {
-                Box(modifier = Modifier.fillMaxSize()) {
-                    LazyColumn {
-                        categoryState.item.forEach { category ->
-                            item {
-                                HomeCategoryList(category = category, navController = navController)
-                            }
-                            item { Spacer(modifier = Modifier.padding(8.dp)) }
-                        }
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(text = stringResource(id = R.string.no_internet))
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    Button(
+                        onClick = {
+                            hasInternet.value = NetworkHandler(context).isNetworkAvailable()
+                        },
+                        modifier = Modifier.wrapContentSize()
+                    ) {
+                        Text(text = stringResource(id = R.string.try_again))
                     }
                 }
             }
