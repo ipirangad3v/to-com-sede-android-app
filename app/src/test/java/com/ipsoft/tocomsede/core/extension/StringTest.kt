2079866d43818b37d94cc223cbac01e93f8320b6
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2023-05-09 11:41:19 -0300

    added string tests

---
+++ app/src/test/java/com/ipsoft/tocomsede/core/extension/StringTest.kt
@@ -0,0 +1,44 @@
+package com.ipsoft.tocomsede.core.extension
+
+import com.ipsoft.tocomsede.core.extensions.toCurrency
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class StringTest {
+
+    @Test
+    fun testToCurrencyWithEmptyString() {
+        val result = "".toCurrency()
+        assertEquals("", result)
+    }
+
+    @Test
+    fun testToCurrencyWithNonNumericString() {
+        val result = "abc".toCurrency()
+        assertEquals("", result)
+    }
+
+    @Test
+    fun testToCurrencyWithIntegerString() {
+        val result = "100".toCurrency()
+        assertEquals("R$ 100,00", result)
+    }
+
+    @Test
+    fun testToCurrencyWithDecimalString() {
+        val result = "100.50".toCurrency("$")
+        assertEquals("$ 100,50", result)
+    }
+
+    @Test
+    fun testToCurrencyWithLargeDecimalString() {
+        val result = "100.1234567".toCurrency("R$")
+        assertEquals("R$ 100,12", result)
+    }
+
+    @Test
+    fun testToCurrencyWithInvalidCurrencySymbol() {
+        val result = "100".toCurrency("INVALID")
+        assertEquals("INVALID 100,00", result)
+    }
+}
