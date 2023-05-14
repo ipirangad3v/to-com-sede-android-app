c931887d8af0dbd86746f467d34b7d50b564cd54
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2023-05-14 02:50:38 -0300

    fixed navigation from cart to orders

--- app/src/main/java/com/ipsoft/tocomsede/MainActivity.kt
+++ app/src/main/java/com/ipsoft/tocomsede/MainActivity.kt
@@ -193,7 +193,19 @@ class MainActivity : ComponentActivity() {
                                         launchLoginActivity(isDarkTheme)
                                     },
                                     onCheckoutSuccess = {
-                                        navController.navigate(Screen.Orders.route)
+                                        navController.navigate(Screen.Orders.route) {
+                                            // Pop up to the start destination of the graph to
+                                            // avoid building up a large stack of destinations
+                                            // on the back stack as users select items
+                                            popUpTo(navController.graph.findStartDestination().id) {
+                                                saveState = true
+                                            }
+                                            // Avoid multiple copies of the same destination when
+                                            // reselecting the same item
+                                            launchSingleTop = true
+                                            // Restore state when reselecting a previously selected item
+                                            restoreState = true
+                                        }
                                     }
                                 )
                             }
