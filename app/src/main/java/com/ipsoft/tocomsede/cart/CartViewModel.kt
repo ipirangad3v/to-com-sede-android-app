d7501be892bb8c5684faca1b2eaee886d09ec2ce
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2023-05-14 02:50:43 -0300

    fixed navigation from cart to orders

--- app/src/main/java/com/ipsoft/tocomsede/cart/CartViewModel.kt
+++ app/src/main/java/com/ipsoft/tocomsede/cart/CartViewModel.kt
@@ -17,8 +17,8 @@ import com.ipsoft.tocomsede.data.firebaserealtimedb.phone.RealtimePhoneRepositor
 import com.ipsoft.tocomsede.utils.Cart
 import com.ipsoft.tocomsede.utils.UserInfo
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
 class CartViewModel @Inject constructor(
