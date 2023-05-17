0c11fa53582e40ddd32b8b9ea6db396209033015
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2023-05-17 19:30:07 -0300

    Fixed notifications

--- admin/src/main/java/com/ipsoft/tocomsedeadmin/base/util/Constants.kt
+++ admin/src/main/java/com/ipsoft/tocomsedeadmin/base/util/Constants.kt
@@ -2,7 +2,7 @@ package com.ipsoft.tocomsedeadmin.base.util
 
 object Constants {
 
-    const val NOTIFICATION_CHANNEL_ID = "yZkrd0FCmbMTyibj8lNT"
+    const val NOTIFICATION_CHANNEL_ID = "yZkrd0FCmbMTyibj8c2m300XMdLCZMmc204jwmsmcxlNT"
     const val NOTIFICATION_CHANNEL_NAME = "Firebase Realtime Database"
     const val NOTIFICATION_CHANNEL_DESCRIPTION = "Notificações do Firebase Realtime Database"
 }
